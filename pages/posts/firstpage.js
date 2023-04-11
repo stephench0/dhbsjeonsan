3a708f9419a9a149b3f4aa2b8b8f2ee4bebed4c2
dhbsjeonsan <chosuwoong@naver.com>
2023-04-11 11:21:28 +0900

    first commit

---
+++ pages/posts/firstpage.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+export default function FirstPage() {
+    return (
+        <div>
+            <h1> Hi my name is Suwoong Cho. </h1>
+            <h2>I'm a student in Hanyang University.
+                I am 23 years old and my major is information system.</h2>
+            <h2>My hobby is watching movies alone and I love playing video games.</h2>    
+            <h3>I am very honored to learn Computer Science.</h3>
+            <Link href="/">Back to home</Link>
+        </div>
+    )
+}
